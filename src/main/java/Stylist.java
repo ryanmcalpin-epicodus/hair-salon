356c6cfc6c058b237ba8cd758676da65c9ea1c9f
Ryan McAlpin <rhmcalpin@gmail.com>
2017-03-31 09:08:53 -0700

    pass instantiation test

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,9 +1,23 @@
 import org.sql2o.*;
 
 public class Stylist {
+  private int id;
+  private String name;
+  private String workDays;
 
-  public Stylist() {
-
+  public Stylist(String name, String workDays) {
+    this.name = name;
+    this.workDays = workDays;
   }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public String getWorkDays() {
+    return this.workDays;
+  }
+
+
 
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -12,9 +12,29 @@ public class StylistTest {
   @After
   public void tearDown() {
     try (Connection con = DB.sql2o.open()) {
-      String sql = "DELETE FROM name_of_your_table *;";
+      String sql = "DELETE FROM stylists *;";
       con.createQuery(sql).executeUpdate();
     }
+  }
+
+  @Test
+  public void Stylist_instantiatesCorrectly_true() {
+    Stylist sty = new Stylist("Ryan", "Monday, Friday");
+    assertTrue(sty instanceof Stylist);
+  }
+
+  @Test
+  public void getters_returnsCorrectly_true() {
+    Stylist sty = new Stylist("Ryan", "Monday, Friday");
+    assertEquals("Ryan", sty.getName());
+    assertEquals("Monday, Friday", sty.getWorkDays());
+  }
+
+  @Test
+  public void save_savesToDatabase_true() {
+    Stylist sty = new Stylist("Ryan", "Monday, Friday");
+    sty.save();
+    assertTrue(Stylist.all().get(0).equals(sty));
   }
 
 }
